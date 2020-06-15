e19efba171938eb48e11f93ac620ac6468006818
Indra Gunawan <indra@Indras-iMac.local>
2020-06-15 12:12:56 +0700

    Added Page sample

--- src/bootstrap/atlaskit.jsx
+++ src/bootstrap/atlaskit.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
+// import React from 'react';
 
 export default function (props) {
-  return (
-    <div>{props.children}</div>
-  )
+  return props.children;
 }
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import Page, { Grid, GridColumn } from '@atlaskit/page';
 import Button from '@atlaskit/button';
 
 export default function () {
   return (
-    <Button appearance="primary">Button</Button>
+    <Page>
+      <Grid>
+        <GridColumn>
+          <h2>Header</h2>
+        </GridColumn>
+      </Grid>
+      <Grid>
+        <GridColumn medium={2}>
+          <h2>Sidebar</h2>
+        </GridColumn>
+        <GridColumn medium={10}>
+          <h2>Body</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Blanditiis voluptatum perspiciatis doloribus dignissimos accusamus
+            commodi, nobis ut, error iusto, quas vitae nesciunt consequatur
+            possimus labore! Mollitia est quis minima asperiores.
+          </p>
+          <Button appearance="primary">Button</Button>
+        </GridColumn>
+      </Grid>
+    </Page>
   );
 }
